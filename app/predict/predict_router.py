84ffc38d7f4074532146233d39fac564902809fe
crystalball <khyunjae10@gmail.com>
2023-11-10 09:40:01 +0900

    change model lgbm

--- app/predict/predict_router.py
+++ app/predict/predict_router.py
@@ -2,13 +2,16 @@ import json
 import joblib
 from datetime import datetime
 import numpy as np
+import pandas as pd
 from pydantic import BaseModel, Field
 from fastapi import APIRouter, Request
+from scipy.special import inv_boxcox
+
 from app.utils.logger_utils import Logger
 
 
 router = APIRouter()
-MODEL = joblib.load('models/model.pkl')
+MODEL = joblib.load('models/model_lgbm.pkl')
 LOGGER = Logger()
 
 
@@ -17,27 +20,20 @@ class InputModel(BaseModel):
     plantcode_L: int = Field(default=0)
     week_no: int = Field(default=36)
     waitingno: int = Field(default=73)
-    week_in: int = Field(default=1)
+    week_int: int = Field(default=1)
     dayno: int = Field(default=7)
     daytype: int = Field(default=2)
+    regihour: int = Field(default=11)
+    regiminute: int = Field(default=30)
     regisecond: int = Field(default=44735)
     teamahead: int = Field(default=6)
     customercnt: int = Field(default=2)
     customergroupcnt: int = Field(default=2)
-    seat_2: int = Field(default=24)
-    seat_4: int = Field(default=34)
-    seat_6: int = Field(default=18)
-    seat_8: int = Field(default=0)
-    seat_12: int = Field(default=3)
-    temp_avg: float = Field(default=25.8)
-    rainfall_amt: float = Field(default=0.0)
-    humidity: float = Field(default=66.3)
-
 
 class OutputModel(BaseModel):
     """response body"""
     predict: float = Field(
-        2884.167288531745,
+        600,
         description="wating time predict result"
         )
 
@@ -51,15 +47,17 @@ async def predict_wating_time(
     request_body = payload.__dict__
     trace_code = request.state.trace_code
     await logging("request", request_body, trace_code)
-    # make input data
-    payload_values = list(request_body.values())
-    input_data = np.array([payload_values])
+    # make input data   
+    input_data = pd.DataFrame([request_body])
     # execute predict
     predict = MODEL.predict(
         input_data,
         num_iteration=MODEL.best_iteration
         )
-    response_body = {"predict": predict[0]}
+    lambda_value = 0.3229772566823217
+    prediction = inv_boxcox(predict[0], lambda_value)
+
+    response_body = {"predict": prediction}
     await logging("response", response_body, trace_code)
     return response_body
 
